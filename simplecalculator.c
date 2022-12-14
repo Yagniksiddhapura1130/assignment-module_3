1fa803d0e4e264c009160dbd9bf50ed6b5990f7a
Yagniksiddhapura1130 <119499656+Yagniksiddhapura1130@users.noreply.github.com>
2022-12-14 16:58:54 +0530

    Add files via upload

--- simplecalculator.c
+++ simplecalculator.c
@@ -1,32 +1,34 @@
 #include<stdio.h>
 main()
-{   char operator;
-    int num1, num2;
+{
+    int a,b;
+    int answer;
+    int c,d;
+    int answer1;
+    int e,f;
+    int answer2;
+    int g,h;
+    int answer3;
 
-    printf("enter operator (+, -, *, /):");
-    scanf("%c" ,&operator);
-
-    printf("enter two value one by one:");
-    scanf("%d %d", &num1 ,&num2);
-
-    switch (operator)
-
-   { 
-    case '+' :
-    printf("%d + %d = %d",num1, num2 , num1 + num2);
-    break;
-     case '-' :
-    printf("%d - %d = %d",num1, num2 , num1 - num2);
-    break;
-     case '*' :
-    printf("%d * %d = %d",num1, num2 , num1 * num2);
-    break;
-     case '/' :
-    printf("%d / %d = %d",num1, num2 , num1 / num2);
-    break;
-    default :
-    printf("invalid operator");
-    }
+    printf("enter two number for addition :");
+    scanf("%d %d",&a,&b);
+    answer = a+b;
+   
+    printf("%d\n", answer);
+    printf(" enter two number for substraction : ");
+    scanf("%d %d" ,&c, &d);
+    answer1 = c-d;
+    printf("%d\n", answer1);
+    printf(" enter two number for multiplication : ");
+    scanf("%d %d" ,&e, &f);
+    answer2 = e * f;
+    printf("%d\n", answer2);
+      printf(" enter two number for devidion : ");
+    scanf("%d %d" ,&g, &h);
+    answer3 = g / h;
+    printf("%d\n", answer3);
     
     return 0;
+
+
 }
